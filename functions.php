a7d401d53f3b9dff727065944741ab9d05239ee4
Abe <es335ab@gmail.com>
2014-06-14 23:47:56 +0900

    theme編集

--- functions.php
+++ functions.php
@@ -284,10 +284,9 @@ function twentytwelve_content_nav( $html_id ) {
 	$html_id = esc_attr( $html_id );
 
 	if ( $wp_query->max_num_pages > 1 ) : ?>
-		<nav id="<?php echo $html_id; ?>" class="navigation" role="navigation">
-			<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
-			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentytwelve' ) ); ?></div>
-			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?></div>
+		<nav id="<?php echo $html_id; ?>" class="pager-block" role="navigation">
+			<p class="pager-prev"><?php next_posts_link( __( '過去の投稿', 'twentytwelve' ) ); ?></p>
+			<p class="pager-next"><?php previous_posts_link( __( '最近の投稿', 'twentytwelve' ) ); ?></p>
 		</nav><!-- #<?php echo $html_id; ?> .navigation -->
 	<?php endif;
 }
